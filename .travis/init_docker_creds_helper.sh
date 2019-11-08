c8c7d2d51bc1066f0b64f2b4b7b3ff8af080694c
Olivier Clavel <olivier.clavel@gmail.com>
2019-11-08 18:28:41 +0100

    Renable creds helper and change init phase

--- .travis/init_docker_creds_helper.sh
+++ .travis/init_docker_creds_helper.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Initialize a credential helper to protect docker password storage.
-#set -ex
-#
-#curl -fsSL "https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz" | tar zxv
-#chmod +x $(pwd)/docker-credential-pass
-#sudo mv $(pwd)/docker-credential-pass /usr/bin
-#
-#gpg --batch --gen-key <<-EOF
-#%echo Generating a standard key
-#Key-Type: DSA
-#Key-Length: 1024
-#Subkey-Type: ELG-E
-#Subkey-Length: 1024
-#Name-Real: Docker Thoteam
-#Name-Email: ThoTeam@spamstinks.com
-#Expire-Date: 0
-## Do a commit here, so that we can later print "done" :-)
-#%commit
-#%echo done
-#EOF
-#
-#key=$(gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut --delimiter ':' --fields 5)
-#
-#pass init $key
-#
-#mkdir -p ~/.docker
-#
-#cat > ~/.docker/config.json <<-EOF
-#{
-#  "credsStore" : "pass"
-#}
-#EOF
+set -ex
+
+curl -fsSL "https://github.com/docker/docker-credential-helpers/releases/download/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz" | tar zxv
+chmod +x $(pwd)/docker-credential-pass
+sudo mv $(pwd)/docker-credential-pass /usr/bin
+
+$keypass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20 ; echo '')
+
+gpg --batch --gen-key <<-EOF
+%echo Generating a standard key
+%no-protection
+Key-Type: RSA
+Key-Length: 4096
+Subkey-Type: RSA
+Subkey-Length: 4096
+Name-Real: FAKEME
+Name-Email: fake@me.local
+Expire-Date: 0
+%commit
+%echo done
+EOF
+
+pass init FAKEME
+
+mkdir -p ~/.docker
+
+cat > ~/.docker/config.json <<-EOF
+{
+  "credsStore" : "pass"
+}
+EOF
